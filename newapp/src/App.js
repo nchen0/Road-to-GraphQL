804f8c83e7a64f7f266ff3ae390326b624a08d69
nchen0 <nicky.chen0@gmail.com>
2019-01-24 15:59:37 -0500

    profile

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import Profile from "./Profile";
 
 class App extends Component {
   render() {
-    return <div className="App">Hi</div>;
+    return <Profile />;
   }
 }
 
---
+++ newapp/src/Profile/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const GET_CURRENT_USER = gql`
+  {
+    viewer {
+      login
+      name
+    }
+  }
+`;
+
+const Profile = () => (
+  <Query query={GET_CURRENT_USER}>
+    {({ data }) => {
+      console.log("data is: ", data);
+      const { viewer } = data;
+      if (!viewer) {
+        return null;
+      }
+      return (
+        <div>
+          {viewer.name} {viewer.login}
+        </div>
+      );
+    }}
+  </Query>
+);
+export default Profile;
